32413f306a518f63b7a405b1845a6e8e9bbcf60e
dineshkhichar569 <dineshkhichar569@email.com>
2025-08-24 03:34:57 +0530

    Previously solved Problems and currently solving LinkedList

--- src/Linked_List/Doubly_LinkedList.java
+++ src/Linked_List/Doubly_LinkedList.java
@@ -9,7 +9,6 @@ public class Doubly_LinkedList {
         this.size = 0;
     }
 
-
     public void insertFirst(int value){
         Node node = new Node(value);
         node.next = head;
@@ -71,6 +70,63 @@ public class Doubly_LinkedList {
         size++;
     }
 
+    public int deleteFirst(){
+        int val = head.value;
+
+        head = head.next;
+        if(head != null){
+            head.prev = null;
+        }else {
+            tail = null;
+        }
+
+        size--;
+
+        return val;
+    }
+
+    public int deleteLast(){
+        if(size <= 1){
+            return deleteFirst();
+        }
+
+        Node last = get(size - 2);
+        int val = last.next.value;
+        last.next = null;
+        size--;
+
+        return val;
+    }
+
+    public int delete(int index){
+        if(index == 0){
+            return deleteFirst();
+        }
+        if(index == size){
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+
+        int value = prev.next.value;
+
+        prev.next = prev.next.next;
+        prev.next.next.prev = prev;
+
+        size--;
+
+        return value;
+    }
+
+    Node get(int index){
+        Node node = head;
+
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
     public void display(){
         Node node = head;
         Node last = null;
@@ -81,13 +137,13 @@ public class Doubly_LinkedList {
         }
         System.out.println("null");
 
-        System.out.println("Printing in Reverse.....");
-
-        while(last != null){
-            System.out.print(last.value + " -> ");
-            last = last.prev;
-        }
-        System.out.println("Start");
+//        System.out.println("Printing in Reverse.....");
+//
+//        while(last != null){
+//            System.out.print(last.value + " -> ");
+//            last = last.prev;
+//        }
+//        System.out.println("Start");
 
     }
 
@@ -119,7 +175,15 @@ public class Doubly_LinkedList {
         list.insertLast(100);
 
         list.insert(333, 5);
+        list.display();
 
+        System.out.println(list.deleteFirst());
+        list.display();
+
+        System.out.println(list.deleteLast());
+        list.display();
+
+        System.out.println(list.delete(2));
         list.display();
     }
 }
