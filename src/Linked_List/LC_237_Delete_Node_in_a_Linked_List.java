acfd6b49119c04a0b272d916080cc6848e7cfba9
dineshkhichar569 <dineshkhichar569@gmail.com>
2025-08-25 04:36:13 +0530

    LC 237

--- src/Linked_List/LC_237_Delete_Node_in_a_Linked_List.java
+++ src/Linked_List/LC_237_Delete_Node_in_a_Linked_List.java
@@ -1,25 +1,88 @@
 package Linked_List;
 
+
 public class LC_237_Delete_Node_in_a_Linked_List {
-    private ListNode head;
-    private ListNode tail;
-    private int size;
 
-    public LC_237_Delete_Node_in_a_Linked_List(){
-        this.size = 0;
+    private Node head;
+    private Node tail;
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if(tail == null) {
+            tail = head;
+        }
     }
 
-    public void deleteNode(ListNode node) {
+    public void insertLast(int val){
+        if(tail == null){
+            insertFirst(val);
+        } else {
+            Node node = new Node(val);
+            tail.next = node;
+            tail = node;
+        }
+    }
 
+    public int deleteValue(Node node){
+
+        int val = node.value;
+
+        node.value = node.next.value;
+        node.next = node.next.next;
+
+        return val;
     }
 
 
-    private class ListNode{
-        int value;
-        ListNode next;
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 
-        public void ListNode(int value){
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
             this.value = value;
         }
+    }
+
+    public Node find(int value){
+        Node node = head;
+        while(node != null){
+            if(node.value == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        LC_237_Delete_Node_in_a_Linked_List list = new LC_237_Delete_Node_in_a_Linked_List();
+
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+        list.insertFirst(50);
+        list.insertFirst(60);
+
+        list.insertLast(100);
+
+        list.display();
+
+        System.out.println(list.deleteValue(list.find(30)));
+        list.display();
+
+
     }
 }
---
+++ src/Linked_List/Questions/LC_206_Reverse_Linked_List.java
@@ -0,0 +1,206 @@
+package Linked_List.Questions;
+
+public class LC_206_Reverse_Linked_List {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    /// constructor of LL
+    public LC_206_Reverse_Linked_List(){
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if(tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void insertLast(int val){
+        if(tail == null){
+            insertFirst(val);
+        } else {
+            Node node = new Node(val);
+            tail.next = node;
+            tail = node;
+        }
+        size++;
+    }
+
+    public void insert(int val, int index){
+        if(index == 0){
+            insertFirst(val);
+        } else if (index == size) {
+            insertLast(val);
+        } else {
+            Node temp = head;
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;
+            }
+            Node node = new Node(val, temp.next);
+            temp.next = node;
+            size++;
+        }
+    }
+
+
+    public int deletionFirst(){
+        int val = head.value;
+        head = head.next;
+
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public int deletionLast(){
+        if(size <= 1){
+            return deletionFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+
+        size--;
+        return val;
+    }
+
+    public int delete(int index){
+        if(index == 0){
+            return deletionFirst();
+        }
+        if(index == size - 1){
+            return deletionLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+    public int deleteValue(int value){
+        Node prev = findPrevious(value);
+        int d = prev.value;
+        int val = prev.next.value;
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+
+    public Node get(int index){
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public Node find(int value){
+        Node node = head;
+        while(node != null){
+            if(node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public Node findPrevious(int value){
+        Node prev = null;
+        Node node = head;
+
+        while(node != null){
+            if(node.value == value){
+                return prev;
+            }
+            prev = node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void displayReverse(){
+        Node node = head;
+        Node last = null;
+
+        int i = 1;
+        do {
+            last = get(size-1);
+            if (i == 1){
+                head = last;
+            }
+            i++;
+            node = node.next;
+
+            System.out.print(last.value + ", ");
+        } while(node != null);
+
+        System.out.println(head.value);
+
+        while(last != null){
+            System.out.print(last.value + " -> ");
+            last = last.next;
+        }
+        System.out.println("null");
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    private class Node {
+        private int value;
+        private  Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LC_206_Reverse_Linked_List list = new LC_206_Reverse_Linked_List();
+
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+        list.insertFirst(50);
+        list.insertFirst(60);
+
+        list.insertLast(100);
+
+        list.insert(90, 3);
+
+        list.display();
+
+        System.out.println("Reverse......");
+
+        list.displayReverse();
+
+
+    }
+}
