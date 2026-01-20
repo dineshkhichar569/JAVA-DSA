eed3d946a19fbeca9031f508684316645a6af65c
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-01-20 10:36:43 +0530

    Rotate Number completed

---
+++ src/Math/roatateNumber.java
@@ -0,0 +1,23 @@
+package Math;
+
+public class roatateNumber {
+    static void main() {
+        int n = 12345;
+        int r = 2;
+
+        int copy = n;
+        int pow = 1;
+        while(copy != 0){
+            pow = pow * 10;
+            copy = copy/10;
+        }
+        pow = pow/10;
+
+//        45 x 1000 = 45000
+//        45000 + 123 = 45123
+
+        while(n != 0){
+
+        }
+    }
+}
